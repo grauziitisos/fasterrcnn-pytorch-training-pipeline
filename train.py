8e5f41ed936b3cad3e1d4897fab80232b95aa06d
wilpeers <wilpeers@gmail.com>
2023-12-09 04:24:22 +0200

    tireed

--- train.py
+++ train.py
@@ -319,7 +319,7 @@ def main(args):
     if args['weights'] is None:
         print('Building model from scratch...')
         build_model = create_model[args['model']]
-        model = build_model(num_classes=NUM_CLASSES, pretrained=PRETRAINED, device=DEVICE)
+        model = build_model(num_classes=NUM_CLASSES, pretrained=PRETRAINED, device=args['device'])
 
     # Load pretrained weights if path is provided.
     if args['weights'] is not None:
